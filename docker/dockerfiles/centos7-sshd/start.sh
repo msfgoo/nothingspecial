eedc5a4d24161d2fab00cd3a5057178849bd3ac7
msfgoo <msfgoo@gmail.com>
2017-10-18 22:56:53 +0200

    conf files and script added

---
+++ docker/dockerfiles/centos7-sshd/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
+ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N ''
+ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
+
+/usr/sbin/sshd -D
+
