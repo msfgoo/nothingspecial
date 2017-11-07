374d3c1d27dcd4d7045f0bf0f0e52829c0e6e6ac
msfgoo <32910467+msfgoo@users.noreply.github.com>
2017-11-07 11:19:03 +0100

    Create ctrlpvim.sh

---
+++ ctrlpvim.sh
@@ -0,0 +1,9 @@
+# Clone the plugin into a separate directory:
+mkdir ~/.vim
+cd ~/.vim
+git clone https://github.com/ctrlpvim/ctrlp.vim.git bundle/ctrlp.vim
+# Add to your ~/.vimrc:
+grep '~/.vim/bundle/ctrlp.vim' || echo "set runtimepath^=~/.vim/bundle/ctrlp.vim" >> ~/.vimrc
+# Run at Vim's command line:
+# helptags ~/.vim/bundle/ctrlp.vim/doc
+# Restart Vim and check :help ctrlp.txt for usage instructions and configuration details.
