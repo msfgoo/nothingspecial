5b15176474e9a0d87e70c1738709043f361fe216
msfgoo <msfgoo@gmail.com>
2017-11-07 13:14:26 +0000

    refactoring

--- vim/ctrlpvim.sh
+++ vim/ctrlpvim.sh
@@ -3,7 +3,7 @@ mkdir ~/.vim
 cd ~/.vim
 git clone https://github.com/ctrlpvim/ctrlp.vim.git bundle/ctrlp.vim
 # Add to your ~/.vimrc:
-grep '~/.vim/bundle/ctrlp.vim' || echo "set runtimepath^=~/.vim/bundle/ctrlp.vim" >> ~/.vimrc
+grep '~/.vim/bundle/ctrlp.vim' ~/.vimrc || echo "set runtimepath^=~/.vim/bundle/ctrlp.vim" >> ~/.vimrc
 # Run at Vim's command line:
 # helptags ~/.vim/bundle/ctrlp.vim/doc
 # Restart Vim and check :help ctrlp.txt for usage instructions and configuration details.
