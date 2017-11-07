8ebfe1559008fbb517167c3b969ffcb03e9ec4ee
msfgoo <32910467+msfgoo@users.noreply.github.com>
2017-11-07 14:08:27 +0100

    Update NERDTree.sh

--- vim/NERDTree.sh
+++ vim/NERDTree.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-echo "map <F2> :NERDTreeToggle<CR>" >> ~/.vimrc
+mkdir ~/.vim/vimrc/
+echo "map <F2> :NERDTreeToggle<CR>" > ~/.vim/vimrc/nerdtree_vimrc
